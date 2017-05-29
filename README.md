02f4de0b558ac0d55945475ac6ad9a8e335a0d68
Pietro Mascolo <iz4vve@gmail.com>
2017-05-29 20:27:44 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# MAS for ESX cluster monitoring and maintenance
+###### ©2017 Pietro Mascolo - Released under licence GPLv3.
+
+
---
+++ main_loop.py
@@ -0,0 +1,45 @@
+import sys
+assert sys.version_info >= (3, 6), \
+    "Python 3.6 (or above) is required to run the project"
+
+import aiomas
+import agents.callbacks as callback
+import environment.Env as Env
+from agents.Agents import MonitorAgent, FixerAgent, GateKeeper
+from util.tools import get_logger
+
+LOG = get_logger(__name__)
+
+
+def main():
+    num_hosts = 2
+
+    cluster_container = aiomas.Container.create(("localhost", 5554))
+    agents_container = aiomas.Container.create(('localhost', 5555))
+
+    gatekeeper = GateKeeper(agents_container)
+    fixer = FixerAgent(agents_container, gatekeeper.addr)
+    monitor = MonitorAgent(agents_container, fixer.addr)
+
+    callbacks = {
+        "cpu": callback.cpu,
+        "mem": callback.mem,
+        "disk": callback.disk,
+        "fan": callback.cpu,
+        "io": callback.io,
+        "pingable": callback.not_pingable,
+        "sshable": callback.not_sshable
+    }
+    fixer.batch_register(callbacks)
+
+    cluster = Env.ClusterTelemetryAgent(cluster_container, num_hosts, monitor)
+
+    try:
+        aiomas.run(until=cluster.run_cluster())
+    except KeyboardInterrupt:
+        LOG.info("Received SIGKILL, shutting down gently...")
+
+
+###############################################################################
+if __name__ == '__main__':
+    main()
