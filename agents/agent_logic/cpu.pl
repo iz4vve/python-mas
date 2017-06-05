a9e1b9cd7c64a6ac65b40cb5a7fb9dbf3493852b
Pietro Mascolo <iz4vve@gmail.com>
2017-06-05 19:55:12 +0200

    FIXES logic

--- agents/agent_logic/cpu.pl
+++ agents/agent_logic/cpu.pl
@@ -1,12 +1,19 @@
-% pingable(Host).
-% reachable(Host).
-% cpu(Host, Cpu).
+dynamic cpu/2, high_priority/1, pingable/1, reachable/1.
 
 underused(Host) :- cpu(Host, Cpu), Cpu<50.
 overused(Host) :- cpu(Host, Cpu), Cpu>90.
 
-should_turn_off(Host) :- underused(Host), not(requires_ping(Host)), not(requires_ssh(Host)).
-should_move_or_kill_processes :- overused(Host), not(requires_ping(Host)), not(requires_ssh(Host)).
+should_turn_off(Host) :-
+    underused(Host),
+    \+ requires_ping(Host),
+    \+ requires_ssh(Host),
+    \+ high_priority(Host).
+
+should_move_or_kill_processes :-
+    overused(Host),
+    \+ requires_ping(Host),
+    \+ requires_ssh(Host),
+    \+ high_priority(Host).
 
 requires_ping(Host) :- cpu(Host, Cpu), Cpu=0.
 requires_ssh(Host) :- cpu(Host, Cpu), Cpu=0.
